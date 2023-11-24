ea5af8f9b13e13f0f3b153f7de80074f42cdc39a
james-nightlife <sutthiphong.kan@outlook.com>
2023-11-24 09:14:22 +0700

    modified

---
+++ src/error-page.js
@@ -0,0 +1,10 @@
+function ErrorPage(){
+    return(
+        <> 
+            <h1 style={{textAlign : "center"}}>
+                404 Not Found
+            </h1>
+        </>
+    );
+}
+export default ErrorPage;
